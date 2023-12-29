fe508c2a2f4c0a08e9b6c5243216f5d47f9fa664
ericpennachini <eric.pennachini@gmail.com>
2023-12-28 21:01:29 -0300

    optimized imports

--- app/src/main/java/ar/com/ericpennachini/fashiondog/app/ui/component/DetailedInfoButtonRow.kt
+++ app/src/main/java/ar/com/ericpennachini/fashiondog/app/ui/component/DetailedInfoButtonRow.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronRight
-import androidx.compose.material3.*
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
