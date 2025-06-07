87a349136372c6f5b4e27ba3d82ba4066fe02f42
Eric Pennachini <eric.pennachini@gmail.com>
2025-06-07 14:21:08 -0300

    starting organizing dependencies in catalog file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
-        // useIR = true
+        //useIR = true
     }
 
     buildFeatures {
@@ -49,7 +49,7 @@ android {
 
     composeOptions {
         kotlinCompilerExtensionVersion = property("composeVersion") as String
-        kotlinCompilerVersion = property("kotlinVersion") as String
+//        kotlinCompilerVersion = property("kotlinVersion") as String
     }
     namespace = "ar.com.ericpennachini.fashiondog.app"
 
@@ -62,36 +62,37 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.16.0")
+    // Base Android dependencies
+    implementation("androidx.activity:activity-compose:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.5.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0")
-
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
+    implementation("com.google.android.material:material:1.5.0")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.23.0")
 
+    // Compose
+    implementation("androidx.compose.foundation:foundation:1.1.1")
     implementation("androidx.compose.material3:material3:1.0.1")
-    implementation("androidx.compose.ui:ui:1.1.1")
     implementation("androidx.compose.material:material:1.1.1")
     implementation("androidx.compose.material:material-icons-core:1.1.1")
     implementation("androidx.compose.material:material-icons-extended:1.1.1")
-    implementation("androidx.compose.foundation:foundation:1.1.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.1.1")
     implementation("androidx.compose.runtime:runtime-livedata:1.1.1")
+    implementation("androidx.compose.ui:ui:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.1.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.1.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
-    implementation("androidx.activity:activity-compose:1.4.0")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
-
-    implementation("com.google.dagger:hilt-android:2.38.1")
-    kapt("com.google.dagger:hilt-compiler:2.38.1")
-
+    // Hilt DI
     implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
-    kapt("androidx.hilt:hilt-compiler:1.0.0-alpha03")
+    implementation("com.google.dagger:hilt-android:2.38.1")
     annotationProcessor("androidx.hilt:hilt-compiler:1.0.0-alpha03")
+    kapt("com.google.dagger:hilt-compiler:2.38.1")
+    kapt("androidx.hilt:hilt-compiler:1.0.0-alpha03")
 
+    // Room
     implementation("androidx.room:room-runtime:2.5.2")
     implementation("androidx.room:room-ktx:2.5.2")
     kapt("androidx.room:room-compiler:2.5.2")
@@ -101,10 +102,4 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.1")
 
-}
-
-configurations.all {
-    resolutionStrategy {
-        force("androidx.core:core-ktx:1.7.0")
-    }
 }
