49fa04ec3b218b3da3311830820bb2e296d050e9
Eric Pennachini <eric.pennachini@gmail.com>
2025-06-14 12:27:37 -0300

    added simulation of API delay response

--- app/src/main/java/ar/com/ericpennachini/fashiondog/app/data/datasource/MockDataSource.kt
+++ app/src/main/java/ar/com/ericpennachini/fashiondog/app/data/datasource/MockDataSource.kt
@@ -9,7 +9,9 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.delay
 import javax.inject.Inject
+import kotlin.time.Duration
 
 class MockDataSource @Inject constructor(
     @ApplicationContext context: Context
@@ -33,32 +35,42 @@ class MockDataSource @Inject constructor(
     }
 
     override suspend fun addCustomer(customerDTO: CustomerDTO) {
+        delay(DEFAULT_SIMULATED_API_CALL_DELAY)
         customers.add(customerDTO)
     }
 
     override suspend fun getCustomer(id: Long): CustomerDTO? {
+        delay(DEFAULT_SIMULATED_API_CALL_DELAY)
         return customers.find {
             it.id == id
         }
     }
 
     override suspend fun getAllCustomers(): List<CustomerDTO> {
+        delay(DEFAULT_SIMULATED_API_CALL_DELAY)
         return customers.toList()
     }
 
     override suspend fun getCustomerCount(): Int {
+        delay(DEFAULT_SIMULATED_API_CALL_DELAY)
         return customers.size
     }
 
     override suspend fun deleteCustomer(customerDTO: CustomerDTO) {
+        delay(DEFAULT_SIMULATED_API_CALL_DELAY)
         customers.find { it.id == customerDTO.id }?.let {
             customers.remove(it)
         }
     }
 
     override suspend fun editCustomer(customerDTO: CustomerDTO) {
+        delay(DEFAULT_SIMULATED_API_CALL_DELAY)
         customers.replace(customerDTO) {
             it.id == customerDTO.id
         }
+    }
+
+    companion object {
+        private const val DEFAULT_SIMULATED_API_CALL_DELAY: Long = 2000
     }
 }
