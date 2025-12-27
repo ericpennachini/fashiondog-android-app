98feca8da49ef986c5e1ce1e55bacca74293bfb9
ericd <eric.pennachini@gmail.com>
2025-12-27 14:53:32 -0300

    feat: improvement in viewmodel declaration in ViewModelModule.kt

--- app/src/main/java/ar/com/ericpennachini/fashiondog/app/di/ViewModelModule.kt
+++ app/src/main/java/ar/com/ericpennachini/fashiondog/app/di/ViewModelModule.kt
@@ -2,19 +2,10 @@ package ar.com.ericpennachini.fashiondog.app.di
 
 import ar.com.ericpennachini.fashiondog.app.ui.screen.customer.CustomerViewModel
 import ar.com.ericpennachini.fashiondog.app.ui.screen.customerlist.CustomerListViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel {
-        CustomerViewModel(
-            repository = get()
-        )
-    }
-
-    viewModel {
-        CustomerListViewModel(
-            repository = get()
-        )
-    }
+    viewModelOf(::CustomerViewModel)
+    viewModelOf(::CustomerListViewModel)
 }
