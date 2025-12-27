b62caec0bee7498cc15aa381930b9f074959dea4
ericd <eric.pennachini@gmail.com>
2025-12-27 14:40:20 -0300

    fix: fixed some errors in DI

--- app/src/main/java/ar/com/ericpennachini/fashiondog/app/di/AppModule.kt
+++ app/src/main/java/ar/com/ericpennachini/fashiondog/app/di/AppModule.kt
@@ -1,16 +1,11 @@
 package ar.com.ericpennachini.fashiondog.app.di
 
-import android.content.Context
 import ar.com.ericpennachini.fashiondog.app.BaseApplication
-import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val appModule = module {
     single<BaseApplication> {
-        androidContext() as BaseApplication
-    }
-
-    single<Context> {
-        androidContext()
+        androidApplication() as BaseApplication
     }
 }
--- app/src/main/java/ar/com/ericpennachini/fashiondog/app/di/RepositoryModule.kt
+++ app/src/main/java/ar/com/ericpennachini/fashiondog/app/di/RepositoryModule.kt
@@ -1,16 +1,22 @@
 package ar.com.ericpennachini.fashiondog.app.di
 
 import ar.com.ericpennachini.fashiondog.app.data.repository.FashionDogRepository
+import ar.com.ericpennachini.fashiondog.app.data.datasource.dto.CustomerDTO
 import ar.com.ericpennachini.fashiondog.app.data.repository.Repository
 import ar.com.ericpennachini.fashiondog.app.domain.mapper.CustomerMapper
+import ar.com.ericpennachini.fashiondog.app.domain.model.Customer
+import ar.com.ericpennachini.fashiondog.app.domain.mapper.DomainMapper
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single<Repository> {
-        FashionDogRepository(get(), get())
+        FashionDogRepository(
+            dataSource = get(),
+            mapper = get()
+        )
     }
 
-    single {
+    single<DomainMapper<CustomerDTO, Customer>> {
         CustomerMapper
     }
 }
